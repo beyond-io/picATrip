661d7ee9f5eb56c4c78f8177e378b013832f21aa
Shoval Ovadia <shovalovadia94@gmail.com>
2021-05-07 15:58:42 +0300

    Update 'Post' model field and 'Post' form
    - Changed field 'nameOfPoster' to User - Post/models.py, Post/migrations/0001_initial.py, commenting_system/migrations/0001_initial.py modified
    - Update 'CreatePost' form (Removing the 'nameOfPoster' field) - Post/forms.py modified
    - Update 'CreateNewPost' view function according to the change in the form - Post/views.py modified
    
    These changes made to link the user who created the post to the post itself.

--- Post/models.py
+++ Post/models.py
@@ -1,14 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    nameOfPoster = models.CharField(max_length=100)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        null=False,
+    )
+
     nameOfLocation = models.CharField(max_length=100)
     photoURL = models.TextField()
     Description = models.TextField()
 
     def __str__(self):
-        return f'{self.nameOfPoster} traveled {self.nameOfLocation} and wrote: {self.Description}'
+        return f'{self.user.username} traveled {self.nameOfLocation} and wrote: {self.Description}'
 
     @classmethod
     def all_posts(cls):
